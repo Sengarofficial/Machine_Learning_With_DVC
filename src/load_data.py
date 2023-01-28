2cb5815ec32992c27c0d002d02788f883e339332
Sengarofficial <grv.m02@gmail.com>
2023-01-28 16:32:55 +0530

    stage I complete

---
+++ src/load_data.py
@@ -0,0 +1,27 @@
+# get the data from data source 
+# save it in the data/raw for further processed 
+
+import os 
+from get_data import read_params, get_data
+import argparse
+
+
+# loading and save method : this will again expect configuration file 
+
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    # iterate over the previos columns 
+    new_cols = [col.replace(" ", "_") for col in df.columns]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+
+    df.to_csv(raw_data_path, sep=",", index=False, header=new_cols)
+    #df.head()
+
+
+if __name__ == "__main__":
+    # argument passing : like what argument is going to be added 
+    args = argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args = args.parse_args()
+    load_and_save(config_path=parsed_args.config)
